c413992f7ce7195083b8f176baba7669b60ddd60
Chendeli <419287484@qq.com>
2023-11-15 18:26:09 +0800

    更新

--- script/unlink.ts
+++ script/unlink.ts
@@ -1,20 +1,22 @@
 /*
  * @Author: 陈德立*******419287484@qq.com
  * @Date: 2023-11-10 09:39:59
- * @LastEditTime: 2023-11-10 10:29:27
+ * @LastEditTime: 2023-11-15 18:25:44
  * @LastEditors: 陈德立*******419287484@qq.com
  * @Github: https://github.com/Alan1034
  * @Description: unlink处理流程
- * @FilePath: \GeneralBasicForm\script\unlink.ts
+ * @FilePath: \VueNpmFrame\script\unlink.ts
  * 
  */
 import path from "path";
 import fs from 'fs'
 import chalk from 'chalk'
-
+import { fileURLToPath } from 'url'
+const __filenameNew = fileURLToPath(import.meta.url)
+const __dirnameNew = path.dirname(__filenameNew)
 const updateMain = () => {
   try {
-    const packageTxt = fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8');
+    const packageTxt = fs.readFileSync(path.join(__dirnameNew, '../package.json'), 'utf8');
     const packageJson = JSON.parse(packageTxt);
     const umdDir = "./dist/index.umd.js"
     const esDir = "./dist/index.mjs"
@@ -31,7 +33,7 @@ const updateMain = () => {
       "/dist"
     ]
     const versionData = JSON.stringify(packageJson, null, 2);
-    fs.writeFileSync(path.join(__dirname, '../package.json'), versionData, 'utf8');
+    fs.writeFileSync(path.join(__dirnameNew, '../package.json'), versionData, 'utf8');
     console.log(chalk.green.bold('修改入口配置成功！当前入口路径为为：' + umdDir));
   } catch (error) {
     console.log(chalk.red.bold('修改入口配置出错:', error.toString()));
