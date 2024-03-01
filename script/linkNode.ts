178e296e7348723b31f72fb6dcf76bbc354ba33d
Chendeli <419287484@qq.com>
2024-03-01 11:28:00 +0800

    debug

--- script/linkNode.ts
+++ script/linkNode.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: 陈德立*******419287484@qq.com
  * @Date: 2023-11-10 09:39:59
- * @LastEditTime: 2024-03-01 10:41:35
+ * @LastEditTime: 2024-03-01 11:27:02
  * @LastEditors: 陈德立*******419287484@qq.com
  * @Github: https://github.com/Alan1034
  * @Description: link处理流程
@@ -11,4 +11,4 @@
 import { updateMain } from "./initialization";
 
 
-updateMain("./src/index.js")
+updateMain("./dist/src/index.js")
