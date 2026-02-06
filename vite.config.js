4f54954ba8810eb04f9b77d1ba34566c5dd3d76b
Chendeli <419287484@qq.com>
2026-02-06 19:25:46 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import legacy from '@vitejs/plugin-legacy'
 import { fileURLToPath } from 'url'
 import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import react from '@vitejs/plugin-react'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 // import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 import path from 'path';
@@ -75,8 +74,7 @@ export default defineConfig(({ command, mode }) => {
       //     // ...  [See more options](https://github.com/javascript-obfuscator/javascript-obfuscator)
       //   },
       // }),
-      vue(),
-      vueJsx(),
+      react()
     ],
     css: {
       preprocessorOptions: {
